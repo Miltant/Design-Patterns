b6348606081a209dee3cf9c6b515d01c5a8d027a
MiLTanT <baptistemarsigli@gmail.com>
2023-12-19 16:35:55 +0100

    add todo

--- Decorator/index.ts
+++ Decorator/index.ts
@@ -21,3 +21,19 @@ commanderPlate(fourÀCarbonara, [Burrata, SansViande]);
 
 commanderCalzone(fourÀMargherita, [SansFromage]);
 commanderCalzone(fourÀCarbonara);
+
+
+/*************/
+/* Jeu vidéo */
+/*************/
+
+// TODO
+// Dans cet exercice, nous allons créer un système d’arme enchantée pour un jeu en ligne.
+// Chaque arme peut être composée d’un ou plusieurs enchantements ajoutés par le joueur,
+// des enchantements tels que “Enchantements de force” qui donne plus de dégats,
+// “enchantement de feu” qui augmente beaucoup des dégats mais réduits la durabilité,
+// “Enchantement poids plume” qui permet d’augmenter considérablement a vitesse d’attaque mais réduit les dégats.
+
+// Vous pouvez en imaginer autant que vous le souhaitez.
+// L’utilisation du pattern Decorator nous permettra de dynamiquement construire des armes
+// avec enchantements personnalisés en tenant compte des dégâts, la durabilité, et la vitesse d’attaque.
